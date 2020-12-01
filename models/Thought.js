e55865a42e3175d775778333174745f6f4a7493a
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-11-30 17:09:43 -0800

    updated thought.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,6 +1,35 @@
 const { Schema, model } = require('mongoose');
 const dateFormat = require('../utils/dateFormat');
 
+const ReactionSchema = new Schema(
+    {
+        // set custom id to avoid confusion with parent comment _id
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: () => new Types.ObjectId()
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            maxlength: 280
+        },
+        username: {
+            type: String,
+            required: true,
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: createdAtVal => dateFormat(createdAtVal)
+        }
+    },
+    {
+        toJSON: {
+            getters: true
+        }
+    }
+);
+
 const ThoughtSchema = new Schema(
     {
         thoughtText: {
@@ -35,35 +64,6 @@ const Thought = model('Thought', ThoughtSchema);
 ThoughtSchema.virtual('reactionCount').get(function () {
     return this.reactions.length;
 });
-
-const ReactionSchema = new Schema(
-    {
-        // set custom id to avoid confusion with parent comment _id
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId()
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            maxlength: 280
-        },
-        username: {
-            type: String,
-            required: true,
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: createdAtVal => dateFormat(createdAtVal)
-        }
-    },
-    {
-        toJSON: {
-            getters: true
-        }
-    }
-);
 
 // export the User model
 module.exports = Thought;

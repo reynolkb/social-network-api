c59614c25a4fdc80b458e1cc318d9b9739ea7cbf
Kyle Reynolds <kyle.reynolds9146@gmail.com>
2020-12-03 11:50:17 -0800

    got remove reaction working

--- models/Thought.js
+++ models/Thought.js
@@ -37,5 +37,45 @@ thoughtSchema.virtual('reactionCount').get(function () {
 // create the User model using the UserSchema
 const Thought = model('Thought', thoughtSchema);
 
+
 // export the User model
 module.exports = {Thought} ;
+
+// const { Schema, model } = require('mongoose');
+// const reactionSchema = require('./Reaction');
+// const dateFormat = require('../utils/dateFormat');
+
+// const thoughtSchema = new Schema(
+//   {
+//     thoughtText: {
+//       type: String,
+//       required: 'You need to leave a thought!',
+//       minlength: 1,
+//       maxlength: 280
+//     },
+//     createdAt: {
+//       type: Date,
+//       default: Date.now,
+//       get: timestamp => dateFormat(timestamp)
+//     },
+//     username: {
+//       type: String,
+//       required: true
+//     },
+//     reactions: [reactionSchema]
+//   },
+//   {
+//     toJSON: {
+//       getters: true
+//     },
+//     id: false
+//   }
+// );
+
+// thoughtSchema.virtual('reactionCount').get(function() {
+//   return this.reactions.length;
+// });
+
+// const Thought = model('Thought', thoughtSchema);
+
+// module.exports = Thought;
